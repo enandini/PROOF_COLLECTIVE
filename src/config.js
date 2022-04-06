ab8992f5c1a6f260502f2d9a059300c57a5ced4c
Eshita Nandini <enandini@ucmerced.edu>
2022-04-06 10:16:08 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0x6c8984bAf566Db08675310b122BF0be9Ea269ecA",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "My NFT Collection",
+    "title": "PROOF_COLLECTIVE",
 
     // 4. (Optional) Display the floor price chart
     "timeseries_chart": true,
